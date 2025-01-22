68c05afb1e45c3a5373f62d53287c12ad78cf458
jahz2323 <jahzielbelmonte@gmail.com>
2025-01-22 14:17:42 +0000

    - 3D Cube Demo - Updated WebGL_Demo1.js, WebGL Demo1.html, Css - textures : ImageProc.js

--- Project/src/Examples/WebGL_Demo1.js
+++ Project/src/Examples/WebGL_Demo1.js
@@ -1,12 +1,27 @@
+//Mouse position code ---
+const box = document.querySelector('canvas')
+const pageX = document.getElementById('x')
+const pageY = document.getElementById('y')
 
-function main(){
+function UserPos(evt) {
+    // console.log("User Pos X: ", evt.x)
+    // console.log("User Pos Y: ", evt.y)
+    return evt;
+}
+
+box.addEventListener('mousemove', UserPos, false);
+box.addEventListener('mouseenter', UserPos, false);
+box.addEventListener('mouseleave', UserPos, false);
+
+// Canvas code ------------
+function main() {
     const canvas = document.querySelector("#glCanvas");
     const gl = canvas.getContext("webgl");
-    if(gl === null){
+    if (gl === null) {
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
     }
-    _2DTriangle(gl, canvas);
+    Demo(gl, canvas);
 }
 
 const vsGSGL = `
@@ -43,22 +58,78 @@ const fsGSGL = `
 // }`;
 
 
-
-function _2DTriangle(gl, canvas) {
+function Demo(gl, canvas) {
     const vertexdata = [
-        0,0.25,0,
-        0.25,-0.25,0,
-        -0.25,-0.25,0
+        0, 0.25, 0,
+        0.25, -0.25, 0,
+        -0.25, -0.25, 0
     ];
-    const colorData = [
-        1,0,0,1, // red
-        0,1,0,1, //green
-        0,0,1,1 //blue
-    ]
+    const CubeData = [
+
+        // Front
+        0.5, 0.5, 0.5,
+        0.5, -.5, 0.5,
+        -.5, 0.5, 0.5,
+        -.5, 0.5, 0.5,
+        0.5, -.5, 0.5,
+        -.5, -.5, 0.5,
+
+        // Left
+        -.5, 0.5, 0.5,
+        -.5, -.5, 0.5,
+        -.5, 0.5, -.5,
+        -.5, 0.5, -.5,
+        -.5, -.5, 0.5,
+        -.5, -.5, -.5,
+
+        // Back
+        -.5, 0.5, -.5,
+        -.5, -.5, -.5,
+        0.5, 0.5, -.5,
+        0.5, 0.5, -.5,
+        -.5, -.5, -.5,
+        0.5, -.5, -.5,
+
+        // Right
+        0.5, 0.5, -.5,
+        0.5, -.5, -.5,
+        0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        0.5, -.5, 0.5,
+        0.5, -.5, -.5,
+
+        // Top
+        0.5, 0.5, 0.5,
+        0.5, 0.5, -.5,
+        -.5, 0.5, 0.5,
+        -.5, 0.5, 0.5,
+        0.5, 0.5, -.5,
+        -.5, 0.5, -.5,
+
+        // Bottom
+        0.5, -.5, 0.5,
+        0.5, -.5, -.5,
+        -.5, -.5, 0.5,
+        -.5, -.5, 0.5,
+        0.5, -.5, -.5,
+        -.5, -.5, -.5,
+    ];
+
+    function randomColor() {
+        return [Math.random(), Math.random(), Math.random()];
+    }
+
+    let colorData = [];
+    for (let face = 0; face < 6; face++) {
+        let facecolor = randomColor();
+        for (let vertex = 0; vertex < 6; vertex++) {
+            colorData.push(...facecolor);
+        }
+    }
 
     const posbuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, posbuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexdata), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(CubeData), gl.STATIC_DRAW);
 
     const colorbuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, colorbuffer);
@@ -78,7 +149,7 @@ function _2DTriangle(gl, canvas) {
     gl.attachShader(program, vertexshader);
     gl.attachShader(program, fragmentshader);
     gl.linkProgram(program);
-    gl.useProgram(program);
+
     //get attribute in vertex shader
     const positionlocation = gl.getAttribLocation(program, "coordinates");
     gl.enableVertexAttribArray(positionlocation);
@@ -96,31 +167,41 @@ function _2DTriangle(gl, canvas) {
     // Set a random color.
     //gl.uniform4fv(colorLocation, [Math.random(), Math.random(), Math.random(), 1]);
     //requestAnimationFrame(drawScene);
+    gl.useProgram(program);
+    gl.enable(gl.DEPTH_TEST);
 
-    const matrixLocation  = gl.getUniformLocation(program, 'matrix')
-    if(matrixLocation === null){
+    const matrixLocation = gl.getUniformLocation(program, 'matrix')
+    if (matrixLocation === null) {
         console.log("Matrix Location is null");
     }
-
-
     const matrix = glMatrix.mat4.create();
     //console.log(matrix, "Identity Matrix");
-    glMatrix.mat4.translate( matrix, matrix, [-0.2, 0.5, 0.1]);
+    glMatrix.mat4.translate(matrix, matrix, [0.2, 0.5, 0.1]);
     glMatrix.mat4.scale(matrix, matrix, [0.5, 0.5, 0.5]);
+
     //console.log(matrix, "Translate Matrix");
+    //TODO - 3D Translation
+    // const translation = [0,0];
+    // const width = 100;
+    // const height = 50;
+    // // Setup a ui.
+    // webglLessonsUI.setupSlider("#x", {slide: updatePosition(0), max: gl.canvas.width });
+    // webglLessonsUI.setupSlider("#y", {slide: updatePosition(1), max: gl.canvas.height});
+
 
     function animate() {
         requestAnimationFrame(animate);
         gl.useProgram(program)
-        glMatrix.mat4.rotate(matrix, matrix, Math.PI/4/100, [0, 0, 1]);
+        glMatrix.mat4.rotate(matrix, matrix, Math.PI / 4 / 100, [0, 0, 1]);
+        glMatrix.mat4.rotate(matrix, matrix, Math.PI / 4 / 100, [1, 0, 0]);
         gl.uniformMatrix4fv(matrixLocation, false, matrix);
-        // Clear the screen and draw the triangle
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.drawArrays(gl.TRIANGLES, 0, 3);
+        gl.drawArrays(gl.TRIANGLES, 0, CubeData.length / 3);
 
     }
+
     animate();
 
 }
+
 document.addEventListener("DOMContentLoaded", main);
 main();
